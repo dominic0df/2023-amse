bb54980763778b9a3a4a4ead20b0346bd5c5c248
Dominic Fischer <fischer.df@web.de>
2023-05-31 11:58:32 +0200

    add system test

---
+++ project/test/tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python ../data/train_station_analysis_data_pipeline.py
+
+set DB_FILE="../data/train_connection_analysis.sqlite"
+set TIMETABLE_FOR_STATIONS=timetable_for_stations
+set CONNECTION_TIME_GRAPH=connection_time_graph
+
+# Check if 'timetable_for_stations' table of datasource2 exists
+if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='timetable_for_stations';" >/dev/null 2>&1; then
+    echo "The 'timetable_for_stations' table exists."
+else
+    echo "The 'timetable_for_stations' table does not exist."
+fi
+
+# Check if 'connection_time_graph' table of datasource1 exists
+if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='connection_time_graph';" >/dev/null 2>&1; then
+    echo "The 'connection_time_graph' table exists."
+else
+    echo "The 'connection_time_graph' table does not exist."
+fi
