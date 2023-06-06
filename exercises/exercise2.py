9a7e18fad4d06257aca1eca6f17621b441e3200b
Dominic Fischer <fischer.df@web.de>
2023-06-06 13:36:55 +0200

    changed betreiber_nr type

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -19,16 +19,16 @@ def preprocess_data(df):
     df = df[(df['Laenge'] >= -90) & (df['Laenge'] <= 90)]
     df = df[(df['Breite'] >= -90) & (df['Breite'] <= 90)]
     df = df.dropna()
-    print(df.head(5))
     pattern_for_ifopt = r'^\S{2}:\d+:\d+(?:\d+)?$'
     df = df[df['IFOPT'].str.contains(pattern_for_ifopt)]
-    print(df.head(5))
+    df['Betreiber_Nr'] = df['Betreiber_Nr'].astype(int)
     return df
 
 
 pd.options.display.max_colwidth = 100
 pd.options.display.max_columns = 20
-# dtype={'EVA_NR': int, 'DS100': str, 'IFOPT': str, 'NAME': str, 'Verkehr': str, 'Laenge': str, 'Breite': str, 'Betreiber_Name': str, 'Betreiber_Nr': int, 'Status': str}
-df = pd.read_csv(DATASOURCE_URL, sep=";")
-df = preprocess_data(df)
-store_dataframe_in_db(df, TABLE_NAME)
+dataframe = pd.read_csv(DATASOURCE_URL, sep=";")
+dataframe = preprocess_data(dataframe)
+print(dataframe.head(5))
+print(dataframe.dtypes)
+store_dataframe_in_db(dataframe, TABLE_NAME)
