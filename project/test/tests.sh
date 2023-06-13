c8a51354368034cc9daecce2d184b5666dabfcaf
Dominic Fischer <fischer.df@web.de>
2023-06-13 23:11:06 +0200

    add file not exists check

--- project/test/tests.sh
+++ project/test/tests.sh
@@ -7,15 +7,19 @@ set TIMETABLE_FOR_STATIONS=timetable_for_stations
 set CONNECTION_TIME_GRAPH=connection_time_graph
 
 # Check if 'timetable_for_stations' table of datasource2 exists
-if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='timetable_for_stations';" >/dev/null 2>&1; then
-    echo "The 'timetable_for_stations' table exists."
-else
-    echo "The 'timetable_for_stations' table does not exist."
-fi
+if [-f DB_FILE], then
+    if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='timetable_for_stations';" >/dev/null 2>&1; then
+        echo "The 'timetable_for_stations' table exists."
+    else
+        echo "The 'timetable_for_stations' table does not exist."
+    fi
 
-# Check if 'connection_time_graph' table of datasource1 exists
-if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='connection_time_graph';" >/dev/null 2>&1; then
-    echo "The 'connection_time_graph' table exists."
+    # Check if 'connection_time_graph' table of datasource1 exists
+    if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='connection_time_graph';" >/dev/null 2>&1; then
+        echo "The 'connection_time_graph' table exists."
+    else
+        echo "The 'connection_time_graph' table does not exist."
+    fi
 else
-    echo "The 'connection_time_graph' table does not exist."
+    echo "File does not exist"
 fi
