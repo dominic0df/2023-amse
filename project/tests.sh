44b454042d0644e280ff3e633c99be8af89c25b2
Dominic Fischer <fischer.df@web.de>
2023-06-14 13:53:38 +0200

    Make Pipeline Runnable With GitHub Actions - WARNING Pipeline will partly fail as Credentials are needed see comments in tests.sh and README.yml

--- project/tests.sh
+++ project/tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python train_station_analysis_data_pipeline.py
+# remove main/project/ from path to run script locally, this additional path is required for executing the pipeline with GitHub Actions
+python main/project/train_station_analysis_data_pipeline.py
 
-set DB_FILE="data/train_connection_analysis.sqlite"
+# remove main/project/ from path to run script locally, this additional path is required for executing the pipeline with GitHub Actions
+set DB_FILE="main/project/data/train_connection_analysis.sqlite"
 set TIMETABLE_FOR_STATIONS=timetable_for_stations
 set CONNECTION_TIME_GRAPH=connection_time_graph
 
@@ -12,6 +14,9 @@ if [ -e data/train_connection_analysis.sqlite ]; then
     if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='timetable_for_stations';"; then
         echo "The 'timetable_for_stations' table exists."
     else
+        # For calling the DB API an API Key and Client ID is required. Therefore a file auth.yml is required that includes this information
+        # The timetable_for_stations query will be always false in GitHub Actions as this file cannot be provided,
+        # so parts of the pipeline fail as credentials are missing
         echo "The 'timetable_for_stations' table does not exist."
     fi
 
