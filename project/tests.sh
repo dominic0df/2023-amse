187daa2d325b2221a050245fdf47806286466754
Dominic Fischer <fischer.df@web.de>
2023-06-14 13:48:51 +0200

    delete unneeded information

--- project/tests.sh
+++ project/tests.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
+python train_station_analysis_data_pipeline.py
 
-pwd
-ls -la
-cd main
-ls -la
-cd ..
-python main/project/train_station_analysis_data_pipeline.py
-
-set DB_FILE="main/project/data/train_connection_analysis.sqlite"
+set DB_FILE="data/train_connection_analysis.sqlite"
 set TIMETABLE_FOR_STATIONS=timetable_for_stations
 set CONNECTION_TIME_GRAPH=connection_time_graph
 
-cd main/project/data
-ls -la
-
 # Check if DB file exists
-if [ -f DB_FILE ]; then
+if [ -e data/train_connection_analysis.sqlite ]; then
     # Check if 'timetable_for_stations' table of datasource2 exists
-    if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='timetable_for_stations';" >/dev/null 2>&1; then
+    if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='timetable_for_stations';"; then
         echo "The 'timetable_for_stations' table exists."
     else
         echo "The 'timetable_for_stations' table does not exist."
     fi
 
     # Check if 'connection_time_graph' table of datasource1 exists
-    if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='connection_time_graph';" >/dev/null 2>&1; then
+    if sqlite3 "$DB_FILE" "SELECT name FROM sqlite_master WHERE type='table' AND name='connection_time_graph';"; then
         echo "The 'connection_time_graph' table exists."
     else
         echo "The 'connection_time_graph' table does not exist."
