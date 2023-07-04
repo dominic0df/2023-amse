e21ca4ddd07bc1b6829569d4c0454c2c9921a46c
Dominic Fischer <fischer.df@web.de>
2023-07-04 11:50:27 +0200

    solve ex4

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -1,16 +1,34 @@
+import zipfile
+
 import pandas as pd
-import io
-from urllib.request import urlopen
+from urllib.request import urlretrieve
 from sqlalchemy import create_engine
-from zipfile import ZipFile
 
 DATASOURCE_URL = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
+ZIP_FILE_NAME = "data.zip"
 CSV_FILE_NAME = "data.csv"
 TABLE_NAME = "temperatures"
+COLUMNS = ["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)",
+           "Batterietemperatur in °C", "Geraet aktiv"]
+CUSTOM_COLUMN_NAMES_MAP = {"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"}
+
+def extract_data():
+    urlretrieve(DATASOURCE_URL, ZIP_FILE_NAME)
+    with zipfile.ZipFile(ZIP_FILE_NAME) as zipped_data:
+        with zipped_data.open(CSV_FILE_NAME) as csv_data:
+            df = pd.read_csv(csv_data, sep=";", decimal=',', index_col=False,
+                             usecols=COLUMNS)
+            return df
 
 
 def map_celsius_to_fahrenheit(temperature_in_celsius):
     return float((9 / 5) * temperature_in_celsius + 32.0)
+
+
+def validate(df):
+    df = df[df["Geraet"] > 0]
+    df = df[df["Monat"] > 0]
+    return df
 
 
 def store_dataframe_in_db(dataframe, table_name):
@@ -18,12 +36,9 @@ def store_dataframe_in_db(dataframe, table_name):
     dataframe.to_sql(table_name, con=engine, if_exists='replace')
 
 
-csv_file = ZipFile(io.BytesIO(urlopen(DATASOURCE_URL).read()))
-df = pd.read_csv(csv_file.open(CSV_FILE_NAME), sep=";", decimal=',', on_bad_lines='skip', usecols=["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"])
-print(df.head(2))
-df = df.rename(columns={"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"})
+df = extract_data()
+df = df.rename(columns=CUSTOM_COLUMN_NAMES_MAP)
 df["Temperatur"] = df["Temperatur"].apply(map_celsius_to_fahrenheit)
 df["Batterietemperatur"] = df["Batterietemperatur"].apply(map_celsius_to_fahrenheit)
-df = df[df["Geraet"].astype(str).str.isdigit() & df["Geraet"] > 0]
-print(df["Temperatur"])
+df = validate(df)
 store_dataframe_in_db(df, TABLE_NAME)
